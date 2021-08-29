32162b43220e979bc4700e4f2678c54ea4bdf266
voducman <ducman1998@gmail.com>
2021-08-30 00:41:35 +0700

    Update

--- scripts/start_demo_video.sh
+++ scripts/start_demo_video.sh
@@ -5,11 +5,11 @@ export PYTHONPATH=/u01/khienpv1/manvd1/action-recognition/Video-Swin-Transformer
 #checkpoints/swin_tiny_patch244_window877_kinetics400_1k.pth \
 #demo/demo.mp4 \
 #demo/label_map_k400.txt
-python demo/long_video_demo.py configs/recognition/swin/swin_tiny_patch244_window877_kinetics400_1k.py \
+python demo/long_video_demo.py configs/recognition/custom/swin_tiny_patch244_window877_kinetics400_1k_demo.py \
 checkpoints/swin_tiny_patch244_window877_kinetics400_1k.pth \
 demo/video/workout-1.mp4 \
 demo/label_map_k400.txt \
 demo/output/workout-1.mp4 \
---input-step 3 --device cuda --threshold 0.1
+--input-step 4 --device cuda --threshold 0.2
 
 
