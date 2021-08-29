245db98c9d0b31932986b0e2751626794e1a2540
voducman <ducman1998@gmail.com>
2021-08-29 19:53:47 +0700

    Update demo script

--- scripts/start_demo_video.sh
+++ scripts/start_demo_video.sh
@@ -1,11 +1,9 @@
-export CUDA_VISIBLE_DEVICES=1
-conda activate video-swin-transformer
-#python demo/demo.py configs/recognition/swin/swin_tiny_patch244_window877_kinetics400_1k.py \
-#checkpoints/swin_tiny_patch244_window877_kinetics400_1k.pth \
-#demo/demo.mp4 \
-#demo/label_map_k400.txt
+export CUDA_VISIBLE_DEVICES=7
+conda activate open-mmlab
+export PYTHONPATH=/u01/khienpv1/manvd1/action-recognition/Video-Swin-Transformer/mmaction:$PYTHONPATH
+python demo/demo.py configs/recognition/swin/swin_tiny_patch244_window877_kinetics400_1k.py \
+checkpoints/swin_tiny_patch244_window877_kinetics400_1k.pth \
+demo/demo.mp4 \
+demo/label_map_k400.txt
 
-python demo/demo.py configs/recognition/tsn/tsn_r50_video_inference_1x1x3_100e_kinetics400_rgb.py \
-checkpoints/tsn_r50_1x1x3_100e_kinetics400_rgb_20200614-e508be42.pth \
-/u01/khienpv1/manvd1/action-recognition/demo/common/workout-1.mp4 demo/label_map_k400.txt
 
