6bf5e8134401353799f0078b5f68c2a210bc258e
voducman <ducman1998@gmail.com>
2021-08-29 14:33:38 +0700

    Fix

--- start_validate_kinetics_400.sh
+++ start_validate_kinetics_400.sh
@@ -1,5 +1,4 @@
 export CUDA_VISIBLE_DEVICES=1,5,7
 conda activate video-swin-tranformer
-cd mmaction
 bash tools/dist_test.sh configs/recognition/custom/swin_base_patch244_window877_kinetics400_22k.py \
 checkpoints/swin_base_patch244_window877_kinetics400_22k.pth 4 --eval top_k_accuracy
